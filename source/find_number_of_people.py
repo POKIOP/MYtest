a119439852d289fcfba852cdce16bd0f6e5d9770
POKIOP <tomaszjuszczak1@wp.pl>
2022-02-14 18:14:32 +0100

    map function changed on list comprehesion

--- source/find_number_of_people.py
+++ source/find_number_of_people.py
@@ -1,7 +1,7 @@
 import argparse
+import csv
 
-
-def proceed_in_year(csv_file, region, passs, year, sex='both'):
+def proceed_in_year(csv_file, region, passs, year, sex = 'both'):
     """Returns number of people.
 
     Args:
@@ -21,35 +21,70 @@ def proceed_in_year(csv_file, region, passs, year, sex='both'):
 
   
 
-def proceed_all_years(csv_file, region, passs, sex, year ='all'):
-    
+def proceed_all_years(csv_file, region, passs, sex, year = 'all'):
+    """Returns number of people. ZROBIC !!!
+
+    Args:
+        csv_file (list): csv file loaded as list
+        region (str): region of Poland or whole Poland
+        passs (str): pass or not
+        year (int): year of exam
+        sex (str, optional): sex. Defaults to 'both'.
+
+    Returns:
+        int: number of people
+    """
     found_rows = []
     if year == 'all':
         for row in csv_file:
-            if row[0]==region and row[1]==passs and row[2]==sex:
+            if row[0] == region and row[1] == passs and row[2] == sex:
                 found_rows.append(row[4])
-                a=list(map(int, found_rows))  # zmienic na list comprehension, zrobic w return
-        return sum(a)    
+                return sum([int(x) for x in found_rows])
+        ###    
 
         
     else:
         for row in csv_file:
-            if row[0]==region and row[1]==passs and row[2]==sex and row[3]==year:
+            if row[0] == region and row[1] == passs and row[2] == sex and row[3] == year:
                 return row[4]
 
 
-def pass_all_years(csv_file, region, passs, sex, year ='all'):
+def pass_all_years(csv_file, region, passs, sex, year = 'all'):
+    """Returns number of people. ZROBIC !!!
+
+    Args:
+        csv_file (list): csv file loaded as list
+        region (str): region of Poland or whole Poland
+        passs (str): pass or not
+        year (int): year of exam
+        sex (str, optional): sex. Defaults to 'both'.
+
+    Returns:
+        int: number of people
+    """
     found_rows = []
     if year == 'all' and sex == 'both':
         for row in csv_file:
-            if row[0]==region and row[1]==passs:
+            if row[0] == region and row[1] == passs:
                 found_rows.append(row[4])
-                a=list(map(int, found_rows)) # zmienic na list comprehension, zrobic w return
-        return sum(a)
+                return sum([int(x) for x in found_rows])
+        ###
 
-def proceed_vs_pass(csv_file, region, sex='both', year ='all'):
-    zdalo = pass_all_years(csv_file, region, passs='zdało', sex='both', year ='all')
-    przystapilo = pass_all_years(csv_file, region, passs='przystąpiło', sex='both', year ='all')
+def proceed_vs_pass(csv_file, region, sex = 'both', year ='all'):
+    """Returns number of people. ZROBIC !!!
+
+    Args:
+        csv_file (list): csv file loaded as list
+        region (str): region of Poland or whole Poland
+        passs (str): pass or not
+        year (int): year of exam
+        sex (str, optional): sex. Defaults to 'both'.
+
+    Returns:
+        int: number of people
+    """
+    zdalo = pass_all_years(csv_file, region, passs = 'zdało', sex = 'both', year = 'all')
+    przystapilo = pass_all_years(csv_file, region, passs = 'przystąpiło', sex = 'both', year = 'all')
     return zdalo / przystapilo * 100 # 2 liczby po przecinku, znak % lub docs.
     
 
@@ -68,4 +103,4 @@ def proceed_vs_pass(csv_file, region, sex='both', year ='all'):
 #     parser.add_argument('--year', type=int)
 
 #     args=parser.parse_args()
-#     print(find_numb_of_people_0(matura, args.region, args.passs, args.sex, args.year))
+#     print(proceed_in_year(matura, args.region, args.passs, args.sex, args.year))
--- source/main.py
+++ source/main.py
@@ -1,30 +1,30 @@
 #task 1 , result: 15750
-import find_number_of_people
-import d_load_csv
+# import find_number_of_people
+# import d_load_csv
 
-matura=d_load_csv.load_csv('matura.csv')
-print(find_number_of_people.proceed_in_year(matura,region="Lubuskie",passs="przystąpiło", year="2018" ))
+# matura = d_load_csv.load_csv('matura.csv')
+# print(find_number_of_people.proceed_in_year(matura,region = "Lubuskie",passs = "przystąpiło", year = "2018" ))
 
 
 #task 2, result: 77879
 # import find_number_of_people
 # import d_load_csv
 
-# matura=d_load_csv.load_csv('matura.csv')
-# print(find_number_of_people.proceed_all_years(matura,region="Lubuskie", passs="przystąpiło", sex="both"))
+# matura = d_load_csv.load_csv('matura.csv')
+# print(find_number_of_people.proceed_all_years(matura,region = "Lubuskie", passs = "przystąpiło", sex = "both"))
 
 
 #task 3, result: 61883
 # import find_number_of_people
 # import d_load_csv
 
-# matura=d_load_csv.load_csv('matura.csv')
-# print(find_number_of_people.pass_all_years(matura,region="Lubuskie", passs="zdało", sex="both"))
+# matura = d_load_csv.load_csv('matura.csv')
+# print(find_number_of_people.pass_all_years(matura,region = "Lubuskie", passs = "zdało", sex = "both"))
 
 
 #task 4, result: 79.46 %
-# import find_number_of_people
-# import d_load_csv
+import find_number_of_people
+import d_load_csv
 
-# matura=d_load_csv.load_csv('matura.csv')
-# print(find_number_of_people.pass_all_years(matura,region="Lubuskie"))
+matura = d_load_csv.load_csv('matura.csv')
+print(find_number_of_people.pass_all_years(matura,region = "Lubuskie"))
