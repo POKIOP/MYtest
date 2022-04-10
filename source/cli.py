523d3bb50299e181fe66956d7cbd1dfbf7c093cf
POKIOP <tomaszjuszczak1@wp.pl>
2022-04-10 21:37:00 +0200

    adding function quest

--- source/cli.py
+++ source/cli.py
@@ -1,12 +1,12 @@
-import argparse
+# import argparse
 import find_number_of_people
 
 
-def get_parser():
-    parser = argparse.ArgumentParser(description="This program shows you data about results of exams")
-    parser.add_argument("--name", type=str, help="Put your name")
-    args = parser.parse_args()
-    return args
+# def get_parser():
+#     parser = argparse.ArgumentParser(description="This program shows you data about results of exams")
+#     parser.add_argument("--name", type=str, help="Put your name")
+#     args = parser.parse_args()
+#     return args
 
 
 def welcome(name):
@@ -18,12 +18,25 @@ def welcome(name):
     Returns:
         str: welcome text
     """
-    return f"Welcome in my program {name}. Choose the option 1, 2, 3, 4 or 5."
+    return f"""Welcome in my program {name}.
+    Choose the option:
+                1 (Percent of passed)
+                2 ()
+                3
+                4
+                5."""
 
 
 def user_input(text_to_show="Provide text: "):
     value = input(text_to_show)
     return value
+
+
+def quest(question):
+    if question == ("no"):
+        print("Thank you and goodbye.")
+    # else:
+    #     print("What is your name ?")
 
 
 if __name__ == "__main__":
--- source/main.py
+++ source/main.py
@@ -5,25 +5,29 @@ import cli
 
 def main():
     # TODO wywolac funkcje welcome z cli.py, wywolac funkcje get user input,
-    name = cli.user_input("Put your name: ")
+    name = cli.user_input("Hello. Put your name: ")
     print(cli.welcome(name))
     user_choice = cli.user_input("User choice is: ")
     matura = load_csv.load_csv("matura.csv")
 
     if user_choice == "1":
         print(
-            find_number_of_people.percent_of_passed(
+            "The result is: "
+            + find_number_of_people.percent_of_passed(
                 matura,
                 region="Lubuskie",
             )
         )
     elif user_choice == "2":
         print(
-            find_number_of_people.proceed_in_year(
-                matura,
-                passs="zdało",
-                region="Lubuskie",
-                year="2018",
+            "The result is: "
+            + str(
+                find_number_of_people.proceed_in_year(
+                    matura,
+                    passs="zdało",
+                    region="Lubuskie",
+                    year="2018",
+                )
             )
         )
     elif user_choice == "3":
@@ -36,13 +40,8 @@ def main():
                 sex="kobiety",
             )
         )
-    # TODO wywolac funkcje goodbye z cli.py
     question = cli.user_input("Next searching ? yes or no: ")
-    if question == ("no"):
-        print("Thank you and goodbye")
-    else:
-        print(cli.welcome(name))
-        user_choice = cli.user_input("User choice is: ")
+    question = cli.quest(question)
 
 
 if __name__ == "__main__":
